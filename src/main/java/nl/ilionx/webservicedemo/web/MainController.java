1fdd00ee1e431301f238e7dc51dc28787447caec
Anna van den Akker <43809166+avandenakker@users.noreply.github.com>
2018-10-24 10:20:03 +0200

    added thymeleaf
    added thymeleaf

---
+++ src/main/java/nl/ilionx/webservicedemo/web/MainController.java
@@ -0,0 +1,15 @@
+package nl.ilionx.webservicedemo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping(value = { "/", "/index" })
+	public String index(Model model) {
+		return "index.html";
+	}
+
+}
